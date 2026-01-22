1055e018d5d745aa39412f2961d8b787723bca10
egupov.aleksey <egupov.aleksey@mail.ru>
2026-01-22 21:36:27 +0300

    1

--- app/routers/subscriptions.py
+++ app/routers/subscriptions.py
@@ -2,25 +2,27 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from ..database import get_db
-from .. import schemas, crud
+from .. import schemas, crud, security, models
 
 router = APIRouter(prefix="/subs", tags=["subscriptions"])
 
 @router.post("/", response_model=schemas.SubscriptionRead)
 def create_subscription(
     subscription: schemas.SubscriptionCreate,
+    current_user: models.User = Depends(security.get_current_user),
     db: Session = Depends(get_db)
 ):
     try:
-        return crud.create_subscription(db=db, subscription=subscription, user_id=1)  # Временно user_id захардкожен
+        return crud.create_subscription(db=db, subscription=subscription, user_id=current_user.id)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 @router.get("/", response_model=List[schemas.SubscriptionRead])
 def read_user_subscriptions(
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(security.get_current_user)
 ):
     try:
-        return crud.get_user_subscriptions(db=db, user_id=1)  # Временно user_id захардкожен
+        return crud.get_user_subscriptions(db=db, user_id=current_user.id)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
--- app/security.py
+++ app/security.py
@@ -2,8 +2,15 @@ from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
 from jose import jwt
 from config import settings
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
+from sqlalchemy.orm import Session
+from .database import get_db
+from . import crud
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
@@ -21,3 +28,24 @@ def create_access_token(data: dict):
         algorithm=settings.algorithm
     )
     return encoded_jwt
+
+async def get_current_user(
+    db: Session = Depends(get_db),
+    token: str = Depends(oauth2_scheme)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials"
+    )
+    try:
+        payload = jwt.decode(token, settings.secret_key.get_secret_value(), algorithms=[settings.algorithm])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+        
+    user = crud.get_user_by_email(db, email=email)
+    if user is None:
+        raise credentials_exception
+    return user
